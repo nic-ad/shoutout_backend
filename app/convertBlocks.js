19862b1c0044d3bc1550feeb65b523e473b27eb4
Danny <daniel.kim@deptagency.com>
2022-06-24 10:22:00 -0400

    Rename "name" to "text"

--- app/convertBlocks.js
+++ app/convertBlocks.js
@@ -24,7 +24,7 @@ async function convertBlocks({ blocks, client }) {
           uniqueUsers
         );
         outputElements.push({
-          name: recipient.profile.display_name,
+          text: recipient.profile.display_name,
           type: block.type,
         });
         break;
