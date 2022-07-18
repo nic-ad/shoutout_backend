f5b302aaa6360ef0230455f277eda695bd4285b4
Bri Gabiola <brigabiola@outlook.com>
2022-07-18 13:14:03 -0600

    ISSUE-34: install and setup typeorm/models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { PersonModule } from './database/modules/person/person.module';
+import { ChannelModule } from './database/modules/channel/channel.module';
+import { ElementsModule } from './database/modules/elements/elements.module';
+import { MessageModule } from './database/modules/message/message.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PersonModule,
+    ChannelModule,
+    ElementsModule,
+    MessageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
