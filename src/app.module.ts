e844906f693046fc8c269245ce327bedd7645697
Ali Halim <107203116+ali-halim@users.noreply.github.com>
2022-07-21 15:47:20 -0400

    remove folders that were migrated to /modules
    remove double imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { ProfileModule } from './profile/profile.module';
-import { ShoutoutsModule } from './shoutouts/shoutouts.module';
 import { PersonModule } from './database/modules/person/person.module';
 import { ChannelModule } from './database/modules/channel/channel.module';
 import { ElementsModule } from './database/modules/elements/elements.module';
--- src/shoutouts/shoutouts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ShoutoutsController } from './shoutouts.controller';
-import { ShoutoutsService } from './shoutouts.service';
-
-@Module({
-  controllers: [ShoutoutsController],
-  providers: [ShoutoutsService],
-})
-export class ShoutoutsModule {}
