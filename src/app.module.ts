7e1c6040a7035ce3bd5286a695e1fdfe258d774b
Nat Ring <natring@rocketinsights.com>
2022-08-04 10:35:28 -0400

    Remove unneeded providers in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,21 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PersonModule } from './modules/database/person/person.module';
 import { ChannelModule } from './modules/database/channel/channel.module';
 import { ElementsModule } from './modules/database/elements/elements.module';
 import { MessageModule } from './modules/database/message/message.module';
 import { ProfileModule } from './modules/api/profile/profile.module';
 import { ShoutoutsModule } from './modules/api/shoutouts/shoutouts.module';
-import { SlackService } from './slack/slack.service';
-import { messageProviders } from './modules/database/message/message.providers';
 import { DatabaseModule } from './modules/database/database.module';
-import { MessageService } from './modules/database/message/message.service';
-import { PersonService } from './modules/database/person/person.service';
-import { personProviders } from 'src/modules/database/person/person.providers';
 import { SlackModule } from './slack/slack.module';
-import { ChannelService } from './modules/database/channel/channel.service';
-import { channelProviders } from './modules/database/channel/channel.providers';
 
 @Module({
   imports: [
@@ -30,15 +22,6 @@ import { channelProviders } from './modules/database/channel/channel.providers';
     SlackModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    ChannelService,
-    ...channelProviders,
-    MessageService,
-    ...messageProviders,
-    PersonService,
-    ...personProviders,
-    SlackService,
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
