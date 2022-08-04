23c9ba8c322fe3e4a6059327dcc8feaee0db92e7
Nat Ring <natring@rocketinsights.com>
2022-08-04 12:10:41 -0400

    Ensure local dev with socket mode works. Add some comments explaning what's going on

--- src/slack/slack.service.ts
+++ src/slack/slack.service.ts
@@ -25,39 +25,30 @@ export class SlackService {
       endpoints: '/', // Defaults to /slack/events. We already scoped it in main.ts to /slack/events.
     });
 
+    // TODO: Better way to switch between socket mode and events in prod/staging/dev
+
+    // Use this definition for local development
     this.boltApp = new App({
       appToken: process.env.SLACK_APP_TOKEN,
       token: process.env.SLACK_BOT_TOKEN,
-      // For local development, comment out 'receiver' line and include socketMode line
-      receiver: this.receiver,
-      // socketMode: true,
+      signingSecret: process.env.SLACK_SIGNING_SECRET,
+      socketMode: true,
     });
 
-    const shoutoutExpression = new RegExp(process.env.SHOUTOUT_PATTERN, 'i');
-    const logExpression = new RegExp(`^${process.env.LOG_PATTERN}$`, 'i');
-    this.boltApp.message(shoutoutExpression, this.handleMessage.bind(this));
-    this.boltApp.message(logExpression, this.handleLogMessage.bind(this));
-
-    // For local development, uncomment this.boltApp.start()
-    // this.boltApp.start()
-
-    // Explanation:
-    // 'socketMode' is particularly useful for local development. we'll turn it off in staging
-    // but the reason we have to have both this.receiver and this.boltApp is that you can't
-    // have a custom receiver while using socketMode. Otherwise, it should look like this:
-
-    // this.receiver = new ExpressReceiver({
-    //   signingSecret: process.env.SLACK_SIGNING_SECRET,
-    // });
-
+    // Use this definition for staging/production
     // this.boltApp = new App({
     //   appToken: process.env.SLACK_APP_TOKEN,
     //   token: process.env.SLACK_BOT_TOKEN,
     //   receiver: this.receiver,
     // });
 
-    // finally, this.boltApp.start() is used to actually make the connection. Not sure how this will
-    // work after turning socketMode off, but it's worth noting here. All of the above feels hacky
+    const shoutoutExpression = new RegExp(process.env.SHOUTOUT_PATTERN, 'i');
+    const logExpression = new RegExp(`^${process.env.LOG_PATTERN}$`, 'i');
+    this.boltApp.message(shoutoutExpression, this.handleMessage.bind(this));
+    this.boltApp.message(logExpression, this.handleLogMessage.bind(this));
+
+    // Finally, this.boltApp.start() should be uncommented for local development
+    this.boltApp.start();
   }
 
   async fetchChannelNameBySlackId(slackId) {
